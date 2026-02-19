90ce11bbd23268edaa4530d4917ae77f364cfa50
jyotiyadav123 <jy2767638@gmail.com>
2026-02-19 19:02:08 +0530

    code

---
+++ C/Assigment.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x=10, y;
+    y=x;
+    printf("%d\n",y);
+    x+=10;
+    printf("%d\n",x);
+    x-=10;
+    printf("%d\n",x);
+    x*=2;
+    printf("%d\n",x);
+    return 0;
+}
---
+++ C/suffix.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=124;
+    unsigned int b=124U;
+    long int c=124L;
+    long long d=124LL;
+    return 0;
+
+}
